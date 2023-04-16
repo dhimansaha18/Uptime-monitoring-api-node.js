03d8eee01c55436669bdb8dbc7ccbafd464ca168
dhiman71 <dhiman@exabyting.com>
2023-04-17 00:24:33 +0600

    Routing added

--- helpers/handleReqRes.js
+++ helpers/handleReqRes.js
@@ -1,28 +1,58 @@
+// dependencies
 const url = require('url');
 const { StringDecoder } = require('string_decoder');
+const routes = require('../routes');
+const { notFoundHandler } = require('../handlers/routeHandlers/notFoundHandler');
 
+// modue scaffolding
 const handler = {};
 
 handler.handleReqRes = (req, res) => {
-    // response handle
-    // Parse the url
+    // request handling
+    // get the url and parse it
     const parsedUrl = url.parse(req.url, true);
-    const { path } = parsedUrl;
+    const path = parsedUrl.pathname;
     const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    const method = req.method.toLowerCase();
+    const queryStringObject = parsedUrl.query;
+    const headersObject = req.headers;
+
+    const requestProperties = {
+        parsedUrl,
+        path,
+        trimmedPath,
+        method,
+        queryStringObject,
+        headersObject,
+    };
+
     const decoder = new StringDecoder('utf-8');
     let realData = '';
+
+    const chosenHandler = routes[trimmedPath] ? routes[trimmedPath] : notFoundHandler;
+
+    chosenHandler(requestProperties, (statusCode, payload) => {
+        statusCode = typeof statusCode === 'number' ? statusCode : 500;
+        payload = typeof payload === 'object' ? payload : {};
+
+        const payloadString = JSON.stringify(payload);
+
+        // return the final response
+        res.writeHead(statusCode);
+        res.end(payloadString);
+    });
+
     req.on('data', (buffer) => {
         realData += decoder.write(buffer);
     });
+
     req.on('end', () => {
         realData += decoder.end();
+
         console.log(realData);
+        // response handle
+        res.end('Hello world');
     });
-    console.log(trimmedPath);
-    console.log(path);
-    console.log(parsedUrl);
-    // response handle
-    res.end('Hello node.js!!!');
 };
 
 module.exports = handler;
---
+++ routes.js
@@ -0,0 +1,7 @@
+const { sampleHandler } = require('./handlers/routeHandlers/sampleHandler');
+
+const routes = {
+    sample: sampleHandler,
+};
+
+module.exports = routes;
