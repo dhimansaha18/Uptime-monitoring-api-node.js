b9feadabec55f7dddded47aa63594671735d7fd8
dhimansaha18 <partho.saha01@gmail.com>
2023-04-18 20:45:46 +0600

    Deletion of a user Done

--- handlers/routeHandlers/userHandler.js
+++ handlers/routeHandlers/userHandler.js
@@ -92,25 +92,17 @@ handler._users.post = (requestProperties, callback) => {
 };
 
 handler._users.put = (requestProperties, callback) => {
-    const phone =        typeof requestProperties.body.phone === 'string' &&
-        requestProperties.body.phone?.trim().length === 11
-            ? requestProperties.body.phone
-            : false;
+    const phone = typeof requestProperties.body.phone === 'string';
+    requestProperties.body.phone?.trim().length === 11 ? requestProperties.body.phone : false;
 
-    const firstName =        typeof requestProperties.body.firstName === 'string' &&
-        requestProperties.body.firstName?.trim().length > 0
-            ? requestProperties.body.firstName
-            : false;
+    const firstName = typeof requestProperties.body.firstName === 'string';
+    requestProperties.body.firstName?.trim().length > 0 ? requestProperties.body.firstName : false;
 
-    const lastName =        typeof requestProperties.body.lastName === 'string' &&
-        requestProperties.body.lastName.trim().length > 0
-            ? requestProperties.body.lastName
-            : false;
+    const lastName = typeof requestProperties.body.lastName === 'string';
+    requestProperties.body.lastName.trim().length > 0 ? requestProperties.body.lastName : false;
 
-    const password =        typeof requestProperties.body.password === 'string' &&
-        requestProperties.body.password?.trim().length > 0
-            ? requestProperties.body.password
-            : false;
+    const password = typeof requestProperties.body.password === 'string';
+    requestProperties.body.password?.trim().length > 0 ? requestProperties.body.password : false;
 
     if (phone) {
         if (firstName || lastName || password) {
@@ -158,5 +150,38 @@ handler._users.put = (requestProperties, callback) => {
         });
     }
 };
-handler._users.delete = (requestProperties, callback) => {};
+handler._users.delete = (requestProperties, callback) => {
+    // check the phone number if valid
+    const phone =        typeof requestProperties.queryStringObject.phone === 'string' &&
+        requestProperties.queryStringObject.phone?.trim().length === 11
+            ? requestProperties.queryStringObject.phone
+            : false;
+
+    if (phone) {
+        // lookup the user
+        data.read('users', phone, (err1, userData) => {
+            if (!err1 && userData) {
+                data.delete('users', phone, (err2) => {
+                    if (!err2) {
+                        callback(200, {
+                            message: 'User was successfully deleted!',
+                        });
+                    } else {
+                        callback(500, {
+                            error: 'There was a server side error!',
+                        });
+                    }
+                });
+            } else {
+                callback(500, {
+                    error: 'There was a server side error!',
+                });
+            }
+        });
+    } else {
+        callback(400, {
+            error: 'There was a problem in your request!',
+        });
+    }
+};
 module.exports = handler;
