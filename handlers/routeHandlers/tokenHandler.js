80b0e1239f19f0a92d53f6798347f21f7cdb0e58
dhimansaha18 <partho.saha01@gmail.com>
2023-04-26 20:21:25 +0600

    Token Updation Done

--- handlers/routeHandlers/tokenHandler.js
+++ handlers/routeHandlers/tokenHandler.js
@@ -60,11 +60,10 @@ handler._token.post = (requestProperties, callback) => {
 
 handler._token.get = (requestProperties, callback) => {
     // check the id if valid
-    const id =
-        typeof requestProperties.queryStringObject.id === 'string'
-        && requestProperties.queryStringObject.id?.trim().length === 20
-            ? requestProperties.queryStringObject.id
-            : false;
+    const id = typeof requestProperties.queryStringObject.id === 'string';
+    requestProperties.queryStringObject.id?.trim().length === 20
+        ? requestProperties.queryStringObject.id
+        : false;
     if (id) {
         // lookup the token
         data.read('tokens', id, (err, tokenData) => {
@@ -80,6 +79,44 @@ handler._token.get = (requestProperties, callback) => {
     } else {
         callback(404, {
             error: 'Requested token was not found!',
+        });
+    }
+};
+
+handler._token.put = (requestProperties, callback) => {
+    const id =
+        typeof requestProperties.body.id === 'string'
+        && requestProperties.body.id?.trim().length === 20
+            ? requestProperties.body.id
+            : false;
+    const extend = !!(
+        typeof requestProperties.body.extend === 'boolean' && requestProperties.body.extend === true
+    );
+
+    if (id && extend) {
+        data.read('tokens', id, (err1, tokenData) => {
+            const tokenObject = parseJSON(tokenData);
+            if (tokenObject.expires > Date.now() && !err1) {
+                tokenObject.expires = Date.now() + 60 * 60 * 1000;
+                // store the updated token
+                data.update('tokens', id, tokenObject, (err2) => {
+                    if (!err2) {
+                        callback(200);
+                    } else {
+                        callback(500, {
+                            error: 'There was a server side error!',
+                        });
+                    }
+                });
+            } else {
+                callback(400, {
+                    error: 'Token already expired!',
+                });
+            }
+        });
+    } else {
+        callback(400, {
+            error: 'There was a problem in your request',
         });
     }
 };
