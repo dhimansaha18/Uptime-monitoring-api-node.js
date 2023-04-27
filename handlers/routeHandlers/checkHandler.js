5b54bd808df3896921b73a0b021432102884aab6
dhimansaha18 <partho.saha01@gmail.com>
2023-04-27 12:51:26 +0600

    Url Read API

--- handlers/routeHandlers/checkHandler.js
+++ handlers/routeHandlers/checkHandler.js
@@ -138,4 +138,46 @@ handler._check.post = (requestProperties, callback) => {
         });
     }
 };
+handler._check.get = (requestProperties, callback) => {
+    const id =
+        typeof requestProperties.queryStringObject.id === 'string' &&
+        requestProperties.queryStringObject.id.trim().length === 20
+            ? requestProperties.queryStringObject.id
+            : false;
+
+    if (id) {
+        // lookup the check
+        data.read('checks', id, (err, checkData) => {
+            if (!err && checkData) {
+                const token =
+                    typeof requestProperties.headersObject.token === 'string'
+                        ? requestProperties.headersObject.token
+                        : false;
+
+                tokenHandler._token.verify(
+                    token,
+                    parseJSON(checkData).userPhone,
+                    (tokenIsValid) => {
+                        if (tokenIsValid) {
+                            callback(200, parseJSON(checkData));
+                        } else {
+                            callback(403, {
+                                error: 'Authentication failure!',
+                            });
+                        }
+                    }
+                );
+            } else {
+                callback(500, {
+                    error: 'You have a problem in your request',
+                });
+            }
+        });
+    } else {
+        callback(400, {
+            error: 'You have a problem in your request',
+        });
+    }
+};
+ 
 module.exports=handler;
